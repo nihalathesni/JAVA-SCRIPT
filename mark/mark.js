8fee5eb9495b5d30062c18c0b5e764783af36c65
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-09-18 10:29:19 +0530

    JAVA

---
+++ mark/mark.js
@@ -0,0 +1,29 @@
+var a=83;
+
+if(a>=90){
+    console.log("A+")
+} 
+else if(a>=80 && a<90){
+    console.log("A")
+}
+else if(a>=70 && a<80){
+    console.log("B+")
+}
+else if(a>=60 && a<70){
+    console.log("B")
+}
+else if(a>=50 && a<60){
+    console.log("C+")
+}
+else if(a>=40 && a<50){
+    console.log("C")
+}
+else if(a>=30 && a<40){
+    console. log("D+")
+}
+else if(a>=20 && a<30){
+    console.log("D")
+}
+else{
+    console.log("FAIL")
+}
