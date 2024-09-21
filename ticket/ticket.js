5d526973bf18f0175d0f1d1b7c04ca27f0c5121c
NIHALA THESNI.K <nihalathesni14@gmail.com>
2024-09-21 11:27:53 +0530

    JAVA

---
+++ ticket/ticket.js
@@ -0,0 +1,16 @@
+var age=65;
+
+
+
+if(age < 12){
+    console.log("tikect price=5")
+}
+else if(age < 18){
+    console.log("ticket price=10")
+}
+else if(age < 60){
+    console.log("ticket price=20")
+}  
+else{
+    console.log("ticket price=15")
+}
